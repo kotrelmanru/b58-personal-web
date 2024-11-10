322182d4c2c9ea2e9b477ead5526c91ab2d897b2
Kotrel Manurung <kotrel@MacBook-Pro-Kotrel.local>
2024-11-11 00:28:08 +0700

    fixing fix fix 5

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const pg = require("pg");
 module.exports = {
   development: {
     username: process.env.POSTGRES_USER,
-    password: String(process.env.POSTGRES_PASSWORD), // Pastikan password adalah string
+    password: String(process.env.POSTGRES_PASSWORD),
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",
@@ -13,17 +13,17 @@ module.exports = {
   },
   production: {
     username: process.env.POSTGRES_USER,
-    password: String(process.env.POSTGRES_PASSWORD), // Pastikan password adalah string
+    password: String(process.env.POSTGRES_PASSWORD),
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",
     dialectModule: require("pg"),
-    sslmode: require,
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
+    sslmode: "require", // Pastikan SSL diaktifkan dengan sslmode=require
   },
 };
