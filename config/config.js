d484f018bcedc5c7bbc1b05ff891ae17a317d627
Kotrel Manurung <kotrel@MacBook-Pro-Kotrel.local>
2024-11-11 00:22:15 +0700

    fixing fix fix 3

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",
-    dialectModule: pg,
+    dialectModule: require("pg"),
     dialectOptions: {},
   },
   production: {
@@ -17,7 +17,7 @@ module.exports = {
     database: process.env.POSTGRES_DATABASE,
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",
-    dialectModule: pg,
+    dialectModule: require("pg"),
     dialectOptions: {
       ssl: {
         require: true,
